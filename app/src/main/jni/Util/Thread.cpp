c4b55492e28a5129324772d2242b334726903f6b
Trainsound <tyh.sunhan@gmail.com>
2018-04-25 00:15:49 +0900

    Added Class Thread

---
+++ app/src/main/jni/Util/Thread.cpp
@@ -0,0 +1,30 @@
+//
+// Created by lena on 2018. 4. 24..
+//
+
+#include "Thread.h"
+
+void* pthreadCreate(void* arg){
+    Thread *thread = (Thread*)arg;
+    thread->run();
+}
+
+Thread::Thread(void* arg){
+    this->runFlag = true;
+    if(!pthread_create(&(this->thread), NULL, pthreadCreate, arg)){
+        //exit
+        this->runFlag = false;
+    }
+}
+
+Thread::~Thread(){
+    
+}
+
+bool Thread::getRunnableFlag(){
+    return runFlag;
+}
+
+int Thread::threadStop(){
+    this->runFlag = false;
+}
