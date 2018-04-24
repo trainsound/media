694db527e3d28516c960fea55d000b37f55ffddf
Trainsound <tyh.sunhan@gmail.com>
2018-04-24 23:53:07 +0900

    Added function nativePlay and nativeStop

--- app/src/main/java/medialibrary/Medialibrary.java
+++ app/src/main/java/medialibrary/Medialibrary.java
@@ -6,4 +6,6 @@ public class Medialibrary {
     }
     public static native int nativeInit(String url);
     public static native int nativeSetSurface(Object surface);
+    public static native int nativePlay();
+    public static native int nativeStop();
 }
